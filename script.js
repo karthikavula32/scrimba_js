3a1a96f26be3f6d98e0b4c70dc653b7b1a9bfc64
karthikavula32 <karthikavula32@gmail.com>
2022-01-10 15:47:12 +0530

    Passenger Counter 1

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let count = 0;
 function increment(){
     count=count+1;
-    document.getElementById("countershow").innerHTML = count;
+    document.getElementById("countershow").textContent = count;
+}
+function save(){
+    let savedEl = document.getElementById("savedEntries");
+    savedEl.textContent += count + " - ";
+    count = 0
+    document.getElementById("countershow").textContent = 0
 }
